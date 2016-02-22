07279866311872037d70af6a420a5c8ef8ee74bf
Ben Alman <hi@benalman.me>
2016-02-22 13:16:41 -0600

    Updated add fragment, began form creation.

--- BarcodeScanningApp/app/src/main/java/com/example/millner/barcodescanningapp/AddFragment.java
+++ BarcodeScanningApp/app/src/main/java/com/example/millner/barcodescanningapp/AddFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -30,6 +32,11 @@ public class AddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add, container, false);
+        Spinner buildingSpinner = (Spinner) view.findViewById(R.id.building_spinner);
+        ArrayAdapter<CharSequence> buildingAdapter = ArrayAdapter.createFromResource(this.getContext(), R.array.building_array, R.layout.spinner_item);
+        buildingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        buildingSpinner.setAdapter(buildingAdapter);
+
         return view;
     }
 
